b0d1f4dd3ca222c5145ed8c9fd3de26749325c79
Mitchell Petras <mjp509@usask.ca>
2022-04-20 11:35:05 -0600

    feat: basic header / footer

--- routes/main.route.js
+++ routes/main.route.js
@@ -8,20 +8,17 @@ router.get("/", (req, res) => {
 
 router.get("/about", (req, res) => {
 	//TODO: res.render about
-
-	res.send("<h1> About Section </h1>");
+	res.render("about", { title: "About" });
 });
 
-router.get("/gallery", (req, res) => {
-	//TODO: res.render gallery
-
-	res.send("<h1> Gallery </h1>");
+router.get("/services", (req, res) => {
+	//TODO: res.render services
+	res.render("services", { title: "Services" });
 });
 
 router.get("/contact", (req, res) => {
 	//TODO: res.render contact
-
-	res.send("<h1> Contact </h1>");
+	res.render("contact", { title: "Contact Us" });
 });
 
 module.exports = router;
