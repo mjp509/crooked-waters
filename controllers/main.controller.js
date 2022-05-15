2eb2ea1f0afa13d9eaf126748a1b9344f75f06fa
Mitchell Petras <mjp509@usask.ca>
2022-05-15 15:28:13 -0600

    chore: change to trents email

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -24,7 +24,7 @@ const sendEmail = (email, phone, firstname, lastname, subject, message) => {
 			sender: { email: email },
 			to: [
 				{
-					email: "mpetras14@gmail.com", //TODO: set to Trent's email
+					email: "cwlogworks@gmail.com",
 					name: `${firstname} ${lastname}`,
 				},
 			],
